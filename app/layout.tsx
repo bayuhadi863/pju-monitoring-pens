5565adc676bb078097d3d5a156f20d49c4e11aca
Bayu Hadi Leksana <bayuhadi863@gmail.com>
2024-07-18 14:51:04 +0700

    update: change default color scheme

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,7 +36,7 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <head>
-        <ColorSchemeScript defaultColorScheme='auto' />
+        <ColorSchemeScript />
       </head>
       <body className={poppins.className}>
         <MantineProvider
